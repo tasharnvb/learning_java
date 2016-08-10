24f0dcd694ee527e94c4ccf43745080549bb29ee
tasharnvb <tasharn.b@gmail.com>
2016-08-10 17:03:41 +0100

    Add some stuff

---
+++ iTunes2/src/test/java/music/MusicManagerTest.java
@@ -0,0 +1,28 @@
+package music;
+
+import org.junit.Test;
+
+/**
+ * Created by Academy07 on 10/08/2016.
+ */
+public class MusicManagerTest {
+    @Test
+    public void searchByGenreShouldReturnAllTracksOfTheSpecifiedGenre() {
+        //arrange
+        Artist sayoPony = new Artist(1, "Sayonara Ponytail");
+        Track track1 = new Track("New World Symphony", "Sayonara Ponytail", 4.42, Genre.JPop, );
+        Track track2 = new Track();
+        Track track3 = new Track();
+        Track track4 = new Track();
+        Track track5 = new Track();
+
+        // Id, Title, Artist, Length, Genre, Release Date,
+
+        //act
+
+
+        //assert
+
+
+    }
+}
